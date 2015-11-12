0381ca120a63a2f83fb7f0d49bee5caf901456c1
Chetoplez <frusso.d1989@gmail.com>
2015-11-12 17:26:27 +0100

    New player move

--- HookGreatEscape/Assets/Scripts/Player.cs
+++ HookGreatEscape/Assets/Scripts/Player.cs
@@ -20,7 +20,7 @@ public class Player : MonoBehaviour
     public bool Forward { set { forward = value; } }
     
     /* Jump speed */
-    [Range(0,10)]
+    [Range(10,100)]
     public int jump_speed = 1;
 
     /* Velocity direction */
@@ -82,48 +82,22 @@ public class Player : MonoBehaviour
 	
 	void Update () 
     {
-        if (player_status.can_apply_gravity())
-        {
-            apply_gravity();
-            Debug.Log("Can apply gravity...");
-        }
-        calculate_ray_origins();
-        check_if_onground();
-        if (player_status.state == HandlePlayerStatus.PlayerStatus.JUMPING || player_status.state==HandlePlayerStatus.PlayerStatus.ONAIR)
-        {
-            move_vertically();
-        }
-        Debug.Log("Mystate=" + player_status.state);
+       
     }
 
     #endregion
 
     #region Methods
 
-    /* Apply gravity to our character */
-    /* TODO:: gravity is too strong!!! */
-    void apply_gravity() {
-        if (this.velocity.y < (-max_velocity_y))
-            this.velocity.y = -max_velocity_y;
-        else
-            this.velocity.y -=  (gravity_factor*0.2f);
-    }
-
-    /* Add force to our character */
-    public void add_force(Vector2 force)
-    {
-       this.velocity += new Vector3(force.x,force.y,0);
-    }
-
+ 
 
     /* Move by a fixed quantity */
     public void move(bool horizontal=true){
+        Rigidbody2D rigidbody = this.GetComponent<Rigidbody2D>();
         if (horizontal)
-        {
-            velocity.x = 1;
-            velocity.x = (this.forward) ? 1 : -1;
-        }
-        this.transform.Translate(velocity * speed_factor * Time.deltaTime);
+            rigidbody.AddForce(new Vector2((this.forward) ? this.speed_factor : -speed_factor, 0f));
+        else
+            rigidbody.AddForce(new Vector2(0f,jump_speed*10));
     }
 
     /* Move vertically */
@@ -133,64 +107,7 @@ public class Player : MonoBehaviour
 
     /* Jump! */
     public void jump() {
-        velocity.y = 1;
-        Vector2 my_jump = new Vector2(0,jump_speed*Time.deltaTime);
-        add_force(my_jump);
-        player_status.JumpingTime = System.DateTime.Now ;
-        player_status.state = HandlePlayerStatus.PlayerStatus.JUMPING;
-        move(false);
-    }
-
-
-    /* Check if landed */
-    public void check_if_onground() {
-        var is_going_up = velocity.y > 0;
-        var ray_distance = Mathf.Abs(velocity.y) + skin_width;
-        var ray_direction = is_going_up ? Vector2.up : -Vector2.up;
-        var ray_origin = is_going_up ? _raycast_top_left : _raycast_bottom_left;
-
-        //ray_origin.x += velocity.x;
-        ray_distance = skin_width * 1.5f;
-        bool collision_detected = false;
-        for (int i = 0; i < total_vertical_rays; i++)
-        {
-            var ray_vector = new Vector2(ray_origin.x + (i * _horizontal_distance_between_rays), ray_origin.y);
-            Debug.DrawRay(ray_vector, ray_direction * ray_distance, Color.green);
-
-            var raycasthit = Physics2D.Raycast(ray_vector, ray_direction, ray_distance, platform_mask);
-            if (!raycasthit) continue;
-            if (raycasthit.collider.gameObject.name == "Player") continue;
-
-            collision_detected = true;
-
-            
-            player_status.state = (is_going_up) ? HandlePlayerStatus.PlayerStatus.ONAIR : HandlePlayerStatus.PlayerStatus.ONGROUND;
-           
-
-            if (player_status.state == HandlePlayerStatus.PlayerStatus.ONGROUND)
-                velocity.y = 0;
-
-            /* Error case */
-            if (ray_distance < (skin_width + 0.0001f)) break;
-
-        }
-
-        if (!collision_detected)
-        {
-            player_status.state = HandlePlayerStatus.PlayerStatus.ONAIR;
-        }
-    }
-
-    /* Calculate the origin of the raycast */
-    private void calculate_ray_origins()
-    {
-        var size = new Vector2(box_collider.size.x * Mathf.Abs(transform.localScale.x), box_collider.size.y * Mathf.Abs(transform.localScale.y)) / 2;
-        var center = new Vector2(box_collider.offset.x * transform.localScale.x, box_collider.offset.y * transform.localScale.y);
-
-        _raycast_top_left = transform.position + new Vector3(center.x - size.x + skin_width, center.y - size.y - skin_width);
-        _raycast_bottom_right = transform.position + new Vector3(center.x + size.x - skin_width, center.y - size.y + skin_width);
-        _raycast_bottom_left = transform.position + new Vector3(center.x - size.x + skin_width, center.y - size.y + skin_width);
-        _raycast_top_right = transform.position + new Vector3(center.x + size.x - skin_width, center.y - size.y - skin_width);
+        move_vertically();
     }
 
     /*  Create a bubble */
