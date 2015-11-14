0c65df9e8cca01fe72a33561b3db028447dea92e
Chetoplez <frusso.d1989@gmail.com>
2015-11-15 00:05:36 +0100

    Under the sea

---
+++ HookGreatEscape/Assets/Scripts/Fish.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+   
+    /* Life fish in seconds */
+    [Range(1,60)]
+    public float life = 10;
+    [Range(1, 5)]
+    public int speed = 2;
+
+    private Vector3 velocity=new Vector3(1.0f,0f,0f);
+    
+  
+	void Update () {
+        this.transform.Translate(velocity*speed*Time.deltaTime);
+        life -= Time.deltaTime;
+        if (life <= 0)
+            GameObject.Destroy(this.gameObject);
+	}
+}
---
+++ HookGreatEscape/Assets/Scripts/FishGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishGenerator : MonoBehaviour {
+
+    /* Every delta_fish second, a fish will be instantiated */
+    [Range(1, 10)]
+    public int delta_fish = 2;
+    /* Illimitates fish?*/
+    public bool no_limits = true;
+    [Range(1,100)]
+    public int max_fish_number = 100;
+
+    private int fish_generated = 0;
+
+    private float fish_counter;
+
+    void Start() {
+        fish_counter = delta_fish;
+        create_fish();
+    }
+
+    
+	
+	void Update () {
+        fish_counter -= Time.deltaTime;
+        if(!no_limits)
+        {
+            if (fish_generated > fish_counter)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        if (fish_counter <= 0)
+        {
+            fish_counter = delta_fish;
+            create_fish();
+        }
+
+	}
+
+    void create_fish() {
+        Instantiate(Resources.Load("Fish"), this.transform.position, this.transform.rotation);
+        fish_generated++;
+    }
+
+}
