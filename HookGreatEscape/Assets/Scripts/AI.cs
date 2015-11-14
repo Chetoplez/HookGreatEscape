376084a0a4e0d8c3d420970a6a95f545c72f6a6c
Roberta Rappazzo <robertarappazzo@gmail.com>
2015-11-14 18:29:01 +0100

    Risolto bug trema e blocca

--- HookGreatEscape/Assets/Scripts/AI.cs
+++ HookGreatEscape/Assets/Scripts/AI.cs
@@ -84,7 +84,6 @@ public class AI : MonoBehaviour
             pause = 1.5f;
         }
         else {
-            Debug.Log("dovrei flippare");
             flip();
             pause = 1.5f;
         }
@@ -93,7 +92,7 @@ public class AI : MonoBehaviour
 
     public void chasing()
     {
-        transform.position = Vector2.MoveTowards(transform.position,chasingTarget, speed * Time.deltaTime);
+      transform.position = Vector2.MoveTowards(transform.position,chasingTarget, speed * Time.deltaTime);
         //Cambio Sprite
     }
 
@@ -251,15 +250,13 @@ public class AI : MonoBehaviour
                 canThrowing = true;
             }
             else {
-                Debug.Log("Dovrei fare il raycast");
                 Vector2 direction = (transform.localScale.x >= 0 ?Vector2.right : Vector2.left);
                 RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), direction, Mathf.Infinity, layerMask, -Mathf.Infinity, maxDepth);
                 Debug.DrawLine(transform.position, new Vector2(direction.x * 15, transform.position.y));
                 if (hit.collider != null && hit.transform.gameObject.tag == "Hook") {
                     hook = other.gameObject;
                     if (Mathf.Abs(hit.distance) >= minDistanceChaising && Mathf.Abs(hit.distance) < maxDistanceChaising) {
-                        Debug.Log("E dice che è in chasing");
-                        chasingTarget = new Vector2(transform.position.x + hit.transform.position.x + 5, transform.position.y);
+                        chasingTarget = new Vector2( hit.transform.position.x, transform.position.y);
                         canChasing = true;
                         canAttack = false;
                     } else if(hit.distance < minDistanceChaising){
