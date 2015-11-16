669e15788ed8cfd5c6963d6a6b38340ea6c169d7
Roberta Rappazzo <robertarappazzo@gmail.com>
2015-11-16 15:28:42 +0100

    Second Level complete plus prefabs

--- HookGreatEscape/Assets/Scripts/MessageArea.cs
+++ HookGreatEscape/Assets/Scripts/MessageArea.cs
@@ -6,32 +6,15 @@ public class MessageArea : MonoBehaviour {
 
 
     private bool messageShow,read;
-    private string textArea;
-    [Range(1, 5)]
-    public int text;
-    public GameObject gameController;
+    public string textArea;
+    private GameObject gameController;
     public GameObject HUD;
-
-    public void chooseText() {
-        switch (text) {
-            case 1: textArea = "Aiuta Hook a scappare! ASDW per muoverti, barra Spaziatrice per saltare!";
-                break;
-            case 2: textArea = "Le Bottiglie di rum ti aumentano la vita! Clicca per raccoglierla!";
-                break;
-            case 3: textArea = "Per sconfiggere un pirata racchiudilo dentro la bolla! Tasto LMB per sparare,più tieni premuto più la bolla cresce! Attento che non esploda!";
-                    break;
-            case 4:textArea = "Puoi mirare facendo su e giù con il mouse!";
-                break;
-            case 5: textArea = "";
-                break;
-
-}
-    }
 
     // Use this for initialization
     void Start () {
+        gameController = GameObject.Find("GameController");
         messageShow = read = false;
-        chooseText();
+
 	}
 	
 	// Update is called once per frame
